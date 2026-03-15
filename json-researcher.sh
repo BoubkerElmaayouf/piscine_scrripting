9b075ecbcfc60439c77c5ef9860141e0e3b8ed54
boubkerelmaayouf@gmail.com <boubkerelmaayouf@gmail.com>
2026-03-15 03:05:00 +0000

    json-researcher.sh

---
+++ json-researcher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -s https://learn.zone01oujda.ma/assets/superhero/all.json > file.json
+
+jq -r '.. | objects | select(.id? == 1) | "  \"name\": \"\(.name)\",\n    \"power\": \(.powerstats.power)," ' file.json
