02b1040fc74d1fea9565f3c6add8daae46bb2e03
boubkerelmaayouf@gmail.com <boubkerelmaayouf@gmail.com>
2026-03-15 01:43:00 +0000

    head-and-tail.sh

---
+++ head-and-tail.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+curl -s "https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt" | head -n 1
+curl -s "https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt" | tail -n 1
