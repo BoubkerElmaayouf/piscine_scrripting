18b0228d529f32889213839544a748cee06a2b59
boubkerelmaayouf@gmail.com <boubkerelmaayouf@gmail.com>
2026-04-01 14:00:00 +0100

    calculator

---
+++ calculator.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Error: expect 3 arguments"
+    exit 1
+fi
+if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
+    echo "Error: invalid number"
+    exit 4
+fi
+
+if ! [[ $3 =~ ^-?[0-9]+$ ]]; then
+    echo "Error: invalid number"
+    exit 4
+fi
+
+do_mult(){
+    echo $(($1 * $2))
+}
+
+do_divide(){
+    if (( $2 == 0 )); then 
+        echo "Error: division by 0"
+        exit 2
+    fi
+    echo $(($1 / $2))
+}
+
+do_add(){
+    echo $(($1 + $2))
+}
+
+do_sub(){
+    echo $(($1 - $2))
+}
+
+
+
+case "$2" in
+ "*")
+        do_mult "$1" "$3"
+        ;;
+    "+")
+        do_add "$1" "$3"
+        ;;
+ "/")
+        do_divide "$1" "$3"
+        ;;
+    "-")
+        do_sub "$1" "$3"
+        ;;
+  *)
+        echo "Error: invalid operator"
+        exit 3
+        ;;
+esac
